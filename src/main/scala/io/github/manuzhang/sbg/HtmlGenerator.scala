0162dca9eb2a97133c739af469b5482fa7f6d2dc
manuzhang <owenzhang1990@gmail.com>
2020-10-25 17:18:08 +0800

    Add RSS feed

--- src/main/scala/io/github/manuzhang/sbg/HtmlGenerator.scala
+++ src/main/scala/io/github/manuzhang/sbg/HtmlGenerator.scala
@@ -1,5 +1,6 @@
 package io.github.manuzhang.sbg
 
+import java.time.format.DateTimeFormatter
 import java.util.Collection
 
 import com.typesafe.scalalogging.Logger
@@ -21,10 +22,33 @@ object HtmlGenerator {
 }
 
 class HtmlGenerator {
+  val title = "ManuZhang's Blog"
+  val description = "Static Blog generated in Scala"
+  val siteUrl = "https://manuzhang.github.io"
+  val rssFile = "rss/index.xml"
 
-  def genHtml(input: String): Unit = {
-    os.write.over(os.pwd / "index.html",
-      page("themes", genPosts(Path(input), "output")))
+  def genHtml(input: String, output: String): Unit = {
+    val outputPath = Path(output)
+    val postList = genPosts(Path(input))
+
+    postList.foreach { post =>
+      val header = div(h1(post.title), p(post.date, a(cls := "home", href := "../../", "Home")))
+      logger.info(s"Generating ${post.path}")
+      genPost(s"${header}${post.content}", outputPath / "posts" / RelPath(post.path))
+    }
+
+    val toc = ul(
+      postList.map { post =>
+        li(
+          cls := "post-item",
+          p(span(cls := "post-meta", s"${post.date} >> "), post.link))
+      }.toList
+    )
+
+    os.write.over(Path(output) / RelPath(rssFile), rssXml(postList), createFolders = true)
+
+    os.write.over(Path(output) / "index.html",
+      page("themes", toc))
   }
 
   def page(themePath: String, content: Tag): String = {
@@ -54,8 +78,8 @@ class HtmlGenerator {
     head(
       meta(charset := "utf-8"),
       meta(name := "viewport", content := "width=device-width, initial-scale=1.0"),
-      meta(name := "description", content := "A layout example that shows off a blog page with a list of posts."),
-      tag("title")("My Blog"),
+      meta(name := "description", content := description),
+      tag("title")(title),
       link(rel := "shortcut icon", href := "favicon.png"),
       link(rel := "stylesheet", href := "https://unpkg.com/purecss@2.0.3/build/pure-min.css"),
       link(rel := "stylesheet", href := "https://unpkg.com/purecss@2.0.3/build/grids-responsive-min.css"),
@@ -67,8 +91,8 @@ class HtmlGenerator {
 
   def sidebar(): Tag = {
     div(cls := "header",
-      h1(cls := "brand-title", "My Blog"),
-      h2(cls := "brand-tagline", "Static Blog generated in Scala"),
+      h1(cls := "brand-title", title),
+      h2(cls := "brand-tagline", description),
       tag("nav")(cls := "nav",
         ul(cls := "nav-list",
           li(cls := "nav-item",
@@ -76,13 +100,16 @@ class HtmlGenerator {
           ),
           li(cls := "nav-item",
             a(cls := "pure-button", href := "https://twitter.com/manuzhang", "Twitter")
+          ),
+          li(cls := "nav-item",
+            a(cls := "pure-button", href := rssFile, "RSS")
           )
         )
       )
     )
   }
 
-  def genPosts(input: Path, output: String): Tag = {
+  def genPosts(input: Path): List[Post] = {
     val parserOptions = new MutableDataSet()
       .set(Parser.EXTENSIONS,
         List(YamlFrontMatterExtension.create()).asJava.asInstanceOf[Collection[Extension]])
@@ -93,7 +120,7 @@ class HtmlGenerator {
       .toImmutable
     val renderer = HtmlRenderer.builder(rendererOptions).build()
 
-    val postList = ArrayBuffer.empty[Post]
+    val postBuffer = ArrayBuffer.empty[Post]
     val regex = "^(\\d{4})\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])\\-(.*)[.]md".r
 
     def fillPostList(p: Path): Unit = {
@@ -107,7 +134,7 @@ class HtmlGenerator {
               val node = parser.parse(input)
               val dateStr = s"$year-$month-$day"
               val fileName = s"$dateStr-$shortTitle"
-              val outputFile = s"$output/$fileName/index.html"
+              val outputFile = s"$fileName/index.html"
               val content = renderer.render(node)
                 .replace("<code>", "<code class='language-text'>")
 
@@ -123,7 +150,7 @@ class HtmlGenerator {
                   }
                 case _ =>
               }
-              postList += Post(title, content, outputFile, dateStr)
+              postBuffer += Post(title, content, outputFile, dateStr)
             }
 
             case _ =>
@@ -134,17 +161,7 @@ class HtmlGenerator {
     }
 
     fillPostList(input)
-
-    ul(
-      postList.sortBy(_.date).reverse.map { post =>
-        val title = div(h1(post.title), p(post.date, a(cls := "home", href := "../../", "Home")))
-        logger.info(s"Generating ${post.path}")
-        genPost(s"${title.render}${post.content}", os.pwd / RelPath(post.path))
-        li(
-          cls := "post-item",
-          p(span(cls := "post-meta", s"${post.date} >> "), post.link))
-      }.toList
-    )
+    postBuffer.toList.sortBy(_.date).reverse
   }
 
   def footer(): Tag = {
@@ -154,8 +171,32 @@ class HtmlGenerator {
     )
   }
 
+  def rssXml(posts: List[Post]): String = {
+    val snippet = tag("rss")(attr("version") := "2.0")(
+      tag("channel")(
+        tag("title")(title),
+        tag("link")(siteUrl),
+        tag("description")(description),
+
+        for(p <- posts) yield tag("item")(
+          tag("title")(p.title),
+          tag("link")(s"$siteUrl/${p.path}"),
+          tag("description")(p.content),
+          tag("pubDate")(p.rssPubDate),
+        )
+
+      )
+    )
+    """<?xml version="1.0"?>""" + snippet.render
+  }
+
   case class Post(title: String, content: String, path: String, date: String) {
 
     def link: TypedTag[String] = a(href := path, title)
+
+    def rssPubDate: String = {
+      DateTimeFormatter.RFC_1123_DATE_TIME.format(
+        DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(s"${date}T00:00:00+00:00"))
+    }
   }
 }
--- src/main/scala/io/github/manuzhang/sbg/Main.scala
+++ src/main/scala/io/github/manuzhang/sbg/Main.scala
@@ -4,6 +4,7 @@ import org.rogach.scallop.ScallopConf
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val input = opt[String](required = true)
+  val output = opt[String](default = Some((os.pwd / "output").toString()), required = false)
   verify()
 }
 
@@ -11,5 +12,5 @@ object Main extends App {
 
   val conf = new Conf(args)
   val generator = new HtmlGenerator
-  generator.genHtml(conf.input())
+  generator.genHtml(conf.input(), conf.output())
 }
